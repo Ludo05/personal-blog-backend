c393991c3f08db51331b653e8e90e0e9901e708e
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2021-08-23 20:35:06 +0100

    format code structure

--- dist/src/constants/config.js
+++ dist/src/constants/config.js
@@ -1,8 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = __importDefault(require("config"));
 exports.PORT = 9002;
-exports.MONGODB_CONNECTION_STRING = config_1.default.get('mongoDb.connectionString');
+exports.MONGODB_CONNECTION_STRING = "mongodb+srv://admin:admin@cluster0.xpwrw.mongodb.net/blog?retryWrites=true&w=majority";
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
 import config from 'config';
 
 export const PORT: number = 9002;
-export const MONGODB_CONNECTION_STRING: string = config.get('mongoDb.connectionString');
+export const MONGODB_CONNECTION_STRING: string = "mongodb+srv://admin:admin@cluster0.xpwrw.mongodb.net/blog?retryWrites=true&w=majority";
