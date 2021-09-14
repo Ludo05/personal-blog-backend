77fb732beab4ad37b1ab7b9b2f6faa0239cbc1b3
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2021-09-14 23:08:42 +0100

    first commit

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -2,7 +2,7 @@ export const PATHS = {
   INDEX: '/',
   ALL: '/all',
   ITEM: '/item',
-  ITEM_BY_ID: '/item/:id',
+  ITEM_BY_ID: '/item',
   EMAIL_WITH_ATTACHMENT: '/email-with-attachment',
   EMAIL: '/email',
   TEST: '/test'
