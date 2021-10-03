c164eca0511bd6e3892f129949320e17bf0f1933
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2021-10-03 22:48:23 +0100

    added auth

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -1,4 +1,5 @@
 import mongoose, {Model, Schema} from "mongoose";
+import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema({
     username: String,
@@ -6,6 +7,30 @@ const UserSchema = new Schema({
     password: String
 });
 
+UserSchema.pre("save", function (next) {
+    const user = this
+
+    if (this.isModified("password") || this.isNew) {
+        bcrypt.genSalt(10, function (saltError, salt) {
+            if (saltError) {
+                return next(saltError)
+            } else {
+                // @ts-ignore
+                bcrypt.hash(user.password, salt, function(hashError, hash) {
+                    if (hashError) {
+                        return next(hashError)
+                    }
+
+                    // @ts-ignore
+                    user.password = hash
+                    next()
+                })
+            }
+        })
+    } else {
+        return next()
+    }
+})
 
 const LoginSchema = new Schema({
     username: String,
--- src/services/AdminService.ts
+++ src/services/AdminService.ts
@@ -15,10 +15,7 @@ export class AdminService {
             return res.status(400).send(error)
         }
 
-        const encryptedUser: any = value;
-        encryptedUser.password = bcrypt.hashSync(encryptedUser.password, 10);
-
-        const newUser = new SchemaModel(encryptedUser)
+        const newUser = new SchemaModel(req.body)
         newUser.save((error: Error, mongoUser: MongooseDocument) => {
             if (error) {
                 return res.status(400).send(error)
@@ -27,24 +24,24 @@ export class AdminService {
         })
     }
 
-    public async login(req: Request, res: Response, next: NextFunction) {
+    public async login(req: Request, res: Response) {
         const {value, error} = loginValidation.validate(req.body);
         if (error) {
             return res.status(422).send(error)
         }
         const user = await SchemaModel.findOne({username: value.username}).exec();
-        if (!bcrypt.compareSync(value.password, user.password)) {
+
+        const decodedPassword = await bcrypt.compare(req.body.password, user.password)
+        console.log(decodedPassword)
+        if (!decodedPassword) {
             // Should return a error saying the username and password doesnt match.
             return res.status(300).send({msg: 'User creds are incorrect please try again.'});
         }
-
         const token: string = jwt.sign({username: value.username},'testprivatekey', {
             expiresIn: 300 * jwtExpirySeconds
         });
 
         res.cookie('token', token, {maxAge: (jwtExpirySeconds * 600000) * 600});
-        // @ts-ignore
-        req.session.user = user
         return res.json({
             token,
             user: {
