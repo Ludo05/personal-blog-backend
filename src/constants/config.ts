b472dc1fb934ea8aeb882a6a5ec88fb8c01a8c99
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2021-10-07 13:01:56 +0100

    added docker

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,4 +2,4 @@ import {PORT_NUMBER} from "./types";
 
 export const PORT: PORT_NUMBER = process.env.PORT || 5000;
 export const REDIS_PORT: string | number = process.env.REDIS || 6379;
-export const MONGODB_CONNECTION_STRING: string = process.env.MONGO_STRING;
+export const MONGODB_CONNECTION_STRING: string = "mongodb+srv://admin:admin@cluster0.xpwrw.mongodb.net/blog?retryWrites=true&w=majority";
