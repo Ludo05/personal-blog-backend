bf3aaf738f32380342047b98d25421e9633019e3
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2021-10-17 12:16:53 +0100

    added email service

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import App from "./app";
 import { PORT } from "./constants/config";
 
-new App().app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+new App().app.listen(process.env.PORT || PORT, () => console.log(`Listening on port ${PORT}`));
